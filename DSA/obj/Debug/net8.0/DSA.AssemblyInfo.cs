c662c49cf594d711a063d81b606f3122ceffef37
Pranav Goyal <pr945005@dal.ca>
2024-02-13 22:41:03 +0530

    [Pranav] Refactor : changed branch name

--- DSA/obj/Debug/net8.0/DSA.AssemblyInfo.cs
+++ DSA/obj/Debug/net8.0/DSA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06196f66dbcb55265ad25c85d279acd147c49919")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cada1964ca298fa77d65defa2f4b56448b836a48")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
